92c561d32554f98196ecd5e5a077b93f097f7c5f
FineCoders001 <finecoders001@gmail.com>
2022-02-11 15:34:09 +0530

    4

---
+++ lib/EventModels/EventsDetail.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class EventsDetail{
+  late final String eventName;
+  late final String venue;
+  late final String description;
+  late final bool deptLevel;
+  late final DateTime eventDate;
+  late final TimeOfDay eventStartTime;
+
+
+
+  EventsDetail({required this.eventName,required this.venue,required this.description,
+    required this.deptLevel,required this.eventDate,required this.eventStartTime});
+
+
+
+
+}
