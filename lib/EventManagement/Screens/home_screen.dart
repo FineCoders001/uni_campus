4853e3b6fbfd5ae8757830fd64b8cf982344c4ae
FineCoders001 <finecoders001@gmail.com>
2022-02-11 12:20:40 +0530

    commit

--- lib/EventManagement/Screens/home_screen.dart
+++ lib/EventManagement/Screens/home_screen.dart
@@ -14,7 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Unicpus"),
+        title: const Text("Unicampus"),
       ),
       body: Column(
         children: [
