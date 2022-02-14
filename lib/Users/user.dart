f4ad3aaae4a8329593ad69e2135b9013b32b9408
FineCoders001 <finecoders001@gmail.com>
2022-02-14 14:21:15 +0530

    14 feb

---
+++ lib/Users/user.dart
@@ -0,0 +1,9 @@
+class UserProfile{
+
+  final String userName;
+  final String email;
+  final String password;
+
+  UserProfile({required this.userName , required this.email,required this.password});
+
+}
