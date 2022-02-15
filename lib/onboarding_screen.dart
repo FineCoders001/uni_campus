8b6344a8c114bc953e1493aa82d2dff8d1b193e5
FineCoders001 <finecoders001@gmail.com>
2022-02-15 15:26:28 +0530

    commit

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uni_campus/EventManagement/Screens/home_screen.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({Key? key}) : super(key: key);
@@ -36,48 +38,90 @@ class _OnboardingState extends State<Onboarding> {
       //   shadowColor: Colors.transparent,
       // ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: PageView.builder(
-              itemCount: l.length,
-              controller: _controller,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 15, top: 100),
-                      child: Text(l[index]["title"]!,
-                          style: const TextStyle(fontSize: 27)),
+      body: DefaultTextStyle(
+        style: GoogleFonts.ubuntu(color: Colors.black, fontSize: 15),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 7,
+              child: PageView.builder(
+                itemCount: l.length,
+                controller: _controller,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index != l.length - 1) {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15, top: 100),
+                          child: Text(l[index]["title"]!,
+                              style: const TextStyle(fontSize: 27)),
+                        ),
+                        Text(l[index]["subtitle"]!),
+                        Image.asset(l[index]["image"]!),
+                      ],
+                    );
+                  } else {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15, top: 100),
+                          child: Text(l[index]["title"]!,
+                              style: const TextStyle(fontSize: 27)),
+                        ),
+                        Text(l[index]["subtitle"]!),
+                        Image.asset(l[index]["image"]!),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomeScreen()));
+                          },
+                          child: Container(
+                            width: 3 * MediaQuery.of(context).size.width / 4,
+                            padding: const EdgeInsets.all(20),
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(25),
+                              ),
+                              color: Colors.blue,
+                            ),
+                            child: const Center(child: Text("Take me there!")),
+                          ),
+                        )
+                      ],
+                    );
+                  }
+                },
+                onPageChanged: (value) => setState(() {
+                  _currentpage = value;
+                }),
+              ),
+            ),
+            // const SizedBox(
+            //   height: 100,
+            // ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                          l.length, (index) => _builddots(index, _currentpage)),
                     ),
-                    Text(l[index]["subtitle"]!),
-                    Image.asset(l[index]["image"]!),
-                  ],
-                );
-              },
-              onPageChanged: (value) => setState(() {
-                _currentpage = value;
-              }),
+                  )
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(
-                        l.length, (index) => _builddots(index, _currentpage)),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
