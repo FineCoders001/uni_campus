3d39f19980a4488ebf53a32fc65aea4c084c6eca
FineCoders001 <finecoders001@gmail.com>
2022-02-17 13:21:20 +0530

    commit

--- lib/Users/user.dart
+++ lib/Users/user.dart
@@ -1,9 +1,20 @@
-class UserProfile{
-
+class UserProfile {
   final String userName;
   final String email;
   final String password;
+  final String enroll;
+  final String collegename;
+  final String deptname;
+  final String styear;
+  final String enyear;
 
-  UserProfile({required this.userName , required this.email,required this.password});
-
+  UserProfile(
+      {required this.userName,
+      required this.email,
+      required this.password,
+      required this.enroll,
+      required this.collegename,
+      required this.deptname,
+      required this.enyear,
+      required this.styear});
 }
