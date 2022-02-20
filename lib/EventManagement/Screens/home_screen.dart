5d490a2d018e02171b9959856861558b62cb3b21
FineCoders001 <finecoders001@gmail.com>
2022-02-20 10:29:44 +0530

    commit

--- lib/EventManagement/Screens/home_screen.dart
+++ lib/EventManagement/Screens/home_screen.dart
@@ -165,7 +165,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const Onboarding()));
+                          builder: (context) => const OnBoarding()));
                 }),
                 child: Text("Onboarding"),
               ),
