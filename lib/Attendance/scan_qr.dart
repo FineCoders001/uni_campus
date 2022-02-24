61970b858e60664a79d1bf93418359879ee8f4c2
FineCoders001 <finecoders001@gmail.com>
2022-02-24 12:33:28 +0530

    commit

--- lib/Attendance/scan_qr.dart
+++ lib/Attendance/scan_qr.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:flutter/material.dart';
 
 class ScanQR extends StatefulWidget {
@@ -7,9 +8,32 @@ class ScanQR extends StatefulWidget {
   _ScanQRState createState() => _ScanQRState();
 }
 
+var scanres = "";
+late List l = [];
+
 class _ScanQRState extends State<ScanQR> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          IconButton(onPressed: _scan, icon: const Icon(Icons.camera)),
+          Center(child: Text(scanres)),
+          IconButton(onPressed: _add(scanres), icon: const Icon(Icons.add)),
+          //IconButton(onPressed: _view, icon: const Icon(Icons.view_agenda))
+        ],
+      ),
+    );
+  }
+
+  Future<void> _scan() async {
+    final result = await BarcodeScanner.scan();
+    setState(() {
+      scanres = result.rawContent.toString();
+    });
+  }
+
+  _add(String scanres) async {
+    l.add(scanres);
   }
 }
