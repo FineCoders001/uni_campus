5e381525fd88d4b641bc6f40030fed0a68d92880
FineCoders001 <finecoders001@gmail.com>
2022-03-31 16:24:31 +0530

    Improved Scan QR UI

--- lib/Attendance/scan_qr.dart
+++ lib/Attendance/scan_qr.dart
@@ -1,9 +1,14 @@
 import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:uni_campus/Attendance/Models/attend.dart';
 import 'package:uni_campus/Attendance/view_list.dart';
+import 'package:uni_campus/Profile/Screens/profile_screen.dart';
 
-class ScanQR extends StatefulWidget {
+class ScanQR extends StatefulHookConsumerWidget {
   final dynamic department;
   final dynamic semester;
   final dynamic year;
@@ -31,9 +36,11 @@ String scanRes = "";
 late Attend at;
 //var at = Attend(dept: de, year: ye, semester: se);
 
-class _ScanQRState extends State<ScanQR> {
+class _ScanQRState extends ConsumerState<ScanQR> {
+  late Map<String, dynamic> user;
   @override
   void initState() {
+    user = ref.read(userCrudProvider).user;
     at = Attend(
         facultyName: widget.facultyName,
         dept: widget.department,
@@ -69,59 +76,276 @@ class _ScanQRState extends State<ScanQR> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ListTile(
-                title: Text("Faculty : " + at.facultyName.toString()),
+              Padding(
+                padding: const EdgeInsets.all(33.0),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: const [BoxShadow(blurRadius: 4)]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.cover,
+                          width: 96,
+                          height: 96,
+                          imageUrl: user['profilePicture'],
+                          placeholder: (context, url) => ClipOval(
+                            child: Material(
+                              elevation: 5.0,
+                              shape: const CircleBorder(),
+                              clipBehavior: Clip.hardEdge,
+                              color: const Color.fromARGB(255, 65, 198, 255),
+                              child: Center(
+                                child: Text(
+                                  user['userName'][0],
+                                  style: GoogleFonts.ubuntu(
+                                      color: Colors.white,
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                          errorWidget: (context, url, error) => ClipOval(
+                            child: Material(
+                              elevation: 5.0,
+                              shape: const CircleBorder(),
+                              clipBehavior: Clip.hardEdge,
+                              color: const Color.fromARGB(255, 65, 198, 255),
+                              child: Center(
+                                child: Text(
+                                  user['userName'][0],
+                                  style: GoogleFonts.ubuntu(
+                                      color: Colors.white,
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text(
+                          at.facultyName.toString(),
+                          style: const TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "${at.dept.toString()}   ${at.semester}",
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Divider(thickness: 1),
+                      ),
+                      ListTile(
+                        title: Text(" Subject : " + at.subject),
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("Date : " +
+                                DateFormat('dd MMM yyyy')
+                                    .format(DateTime.parse(
+                                        at.date.toString().substring(0, 10)))
+                                    .toString()),
+                            Text("Time : " +
+                                at.date.toString().substring(11, 16))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              ListTile(
-                title: Text("Department : " + at.dept.toString()),
-                subtitle: Text(
-                    "Semester : " + at.semester + " Subject : " + at.subject),
-              ),
-              ListTile(
-                title: Text("Date : " + at.date.toString().substring(0, 10)),
-                subtitle:
-                    Text("Time : " + at.date.toString().substring(11, 16)),
-              ),
+
+              // ListTile(
+              //   title: Text("Faculty : " + at.facultyName.toString()),
+              // ),
+              // ListTile(
+              //   title: Text("Department : " + at.dept.toString()),
+              //   subtitle: Text(
+              //       "Semester : " + at.semester + " Subject : " + at.subject),
+              // ),
+
               // Text(
               //     "${at.dept} ${at.year} ${at.month} ${at.semester} ${at.date} ${at.subject} ${at.facultyName}"),
+
+              const SizedBox(height: 25),
               SizedBox(
                 height: MediaQuery.of(context).size.height / 3,
-                child: ListView(
+                child: Column(
                   children: [
-                    IconButton(
-                      onPressed: _scan,
-                      tooltip: "Capture QR",
-                      icon: const Icon(Icons.camera),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 8),
+                      child: InkWell(
+                        onTap: _scan,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 60, 138, 63),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                'Capture QR',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.camera_alt_outlined,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                    Center(
-                      child: Text(scanRes),
-                    ),
-                    IconButton(
-                        tooltip: "Add",
-                        onPressed: () {
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 8),
+                      child: InkWell(
+                        onTap: () {
                           if (at.map.contains(scanRes)) {
                             ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    duration: Duration(milliseconds: 1500),
-                                    content: Text('Already Added',
-                                        textAlign: TextAlign.center)));
+                              const SnackBar(
+                                duration: Duration(milliseconds: 1500),
+                                content: Text(
+                                  'Already Added',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            );
                           } else {
                             at.map.add(scanRes);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                duration: const Duration(milliseconds: 1500),
+                                content: Text(
+                                  'Added $scanRes to the list',
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            );
                           }
                         },
-                        icon: const Icon(Icons.add)),
-                    IconButton(
-                      tooltip: "See the list",
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) {
-                            return ViewList(d: at);
-                          }),
-                        );
-                      },
-                      icon: const Icon(Icons.view_agenda),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 60, 138, 63),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              scanRes == ""
+                                  ? const Text(
+                                      'Add Enrollment',
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.white),
+                                    )
+                                  : Text(
+                                      'Add: $scanRes',
+                                      style: const TextStyle(
+                                          fontSize: 20, color: Colors.white),
+                                    ),
+                              const Icon(
+                                Icons.person_add_outlined,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 8),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  ViewList(d: at),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 60, 138, 63),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: const [
+                              Text(
+                                'Check Student list',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.list_alt_rounded,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    // IconButton(
+                    //   onPressed: _scan,
+                    //   tooltip: "Capture QR",
+                    //   icon: const Icon(Icons.camera),
+                    // ),
+                    // Center(
+                    //   child: Text(scanRes),
+                    // ),
+                    // IconButton(
+                    //     tooltip: "Add",
+                    //     onPressed: () {
+                    //       if (at.map.contains(scanRes)) {
+                    //         ScaffoldMessenger.of(context).showSnackBar(
+                    //             const SnackBar(
+                    //                 duration: Duration(milliseconds: 1500),
+                    //                 content: Text('Already Added',
+                    //                     textAlign: TextAlign.center)));
+                    //       } else {
+                    //         at.map.add(scanRes);
+                    //       }
+                    //     },
+                    //     icon: const Icon(Icons.add)),
+                    // IconButton(
+                    //   tooltip: "See the list",
+                    //   onPressed: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(builder: (context) {
+                    //         return ViewList(d: at);
+                    //       }),
+                    //     );
+                    //   },
+                    //   icon: const Icon(Icons.view_agenda),
+                    // ),
                   ],
                 ),
               ),
